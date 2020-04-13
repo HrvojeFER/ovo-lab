6fc6fa5b1b5f2e02545f877fb16d633ee8f1ca7d
Hrvoje Jurić <hrvoje.juric.13@gmail.com>
2020-04-13 20:24:00 +0200

    Exercise 2 done halfway. I need to implement recursive calls. This push is just a "save".

---
+++ exercise2/RayTracing/RayTracing/Scene.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Drawing;
+
+namespace raytracing
+{
+    /// <summary>
+    /// Klasa predstvlja scenu kod modela crtanja slike pomocu ray tracinga. Sastoji
+    /// se od izvora svjetlosti i konacnog broja objekata.
+    /// </summary>
+    public class Scene
+    {
+        const int MAXDEPTH = 5; //maksimalna dubina rekurzije
+        private int numberOfObjects;
+        private Sphere[] sphere;
+        private Point lightPosition;
+        private ColorVector backgroundColors = new ColorVector(0, 0, 0);
+        private ColorVector light = new ColorVector((float)1, (float)1, (float)1);
+        private ColorVector ambientLight = new ColorVector((float)0.5, (float)0.5, (float)0.5);
+
+        /// <summary>
+        /// Inicijalni konstruktor koji postavlja poziciju svijetla i parametre svih
+        /// objekata u sceni.
+        /// </summary>
+        /// <param name="lightPosition">pozicija svijetla</param>
+        /// <param name="numberOfObjects">broj objekata u sceni</param>
+        /// <param name="sphereParameters">parametri svih kugli</param>
+        public Scene ( Point lightPosition, int numberOfObjects, SphereParameters[] sphereParameters )
+        {
+            this.lightPosition = lightPosition;
+            this.numberOfObjects = numberOfObjects;
+            sphere = new Sphere[numberOfObjects];
+            for(int i = 0; i < numberOfObjects; i++)
+            {
+                sphere[i] = new Sphere(sphereParameters[i]);
+            }
+        }
+
+        /// <summary>
+        /// Metoda provjerava da li postoji sjena na tocki presjeka. Vraca true ako
+        /// se zraka od mjesta presjeka prema izvoru svjetlosti sjece s nekim objektom.
+        /// </summary>
+        /// <param name="intersection">tocka presjeka</param>
+        /// <returns>true ako postoji sjena u tocki presjeka, false ako ne postoji</returns>
+        private bool shadow ( Point intersection )
+        {
+            var shadowRay = new Ray(intersection, lightPosition);
+
+            for (var i = 0; i < numberOfObjects; ++i) 
+                if (sphere[i].intersection(shadowRay)) 
+                    return true;  
+
+            return false;
+        }
+
+        /// <summary>
+        /// Metoda koja pomocu pracenja zrake racuna boju u tocki presjeka. Racuna se
+        /// osvjetljenje u tocki presjeka te se zbraja s doprinosima osvjetljenja koje
+        /// donosi reflektirana i refraktirana zraka.
+        /// </summary>
+        /// <param name="ray">pracena zraka</param>
+        /// <param name="depth">dubina rekurzije</param>
+        /// <returns>vektor boje u tocki presjeka</returns>
+        public ColorVector traceRay ( Ray ray, int depth )
+        {
+            // Da bi sprječio beskonačnu rekurziju,
+            // upute govore da koristim provjeru maksimalne dubine, pa 
+            // ako sam prešao maksimalnu dubinu, vraćam samo pozadinsku boju.
+            if (depth > MAXDEPTH) return backgroundColors;
+
+            // Posavljam presjek na null prvo tako da kasnije mogu provjeriti
+            // da li presjek postoji.
+            Point intersection = null;
+            
+            // Spremam i sferu s kojom sam našao presjek za računanje doprinosa osvjetljenja.
+            Sphere intersectionSphere = null;
+            
+            // Čuvam udaljenost presjeka od ishodišta zrake
+            // jer zraka najvjerojatnije presjeca mali broj objekata, pa
+            // bi bilo bezveze svaki je put izračunati.
+            var intersectionDistance = double.MaxValue;
+
+            // Da nađem presjek prelazim sve objekte.
+            for(var i = 0 ; i < numberOfObjects ; ++i)
+            {
+                // Prvo provjeravam da li postoji presjek.
+                if (!sphere[i].intersection(ray)) continue;
+                
+                // Ako postoji presjek treba mi udaljenost presjeka od ishodišta zrake.
+                var currentIntersection = sphere[i].getIntersectionPoint();
+                var currentDistance = 
+                    currentIntersection.getDistanceFrom(ray.getStartingPoint());
+
+                // Treba mi presjek s najmanjom udaljenosti, pa
+                // ako je veća trenutačna udaljenost od udaljenosti prije izračunatog presjeka,
+                // ne uzimam trenutačan presjek. 
+                if (currentDistance > intersectionDistance) continue;
+                    
+                intersection = currentIntersection;
+                intersectionDistance = currentDistance;
+                intersectionSphere = sphere[i];
+            }
+
+            // Ako nisam našao presjek vraćam samo pozadinsku boju.
+            if (intersection == null) return backgroundColors;
+
+            // Sad kada sam našao najbliži presjek trebam izračunati doprinose
+            // ove zrake (Phong-ov model - ambijentna, difuzna i spekularna komponenta), te
+            // doprinose reflektiranih i refraktiranih zraka.
+            var colorContributions = new ColorVector(0.0f, 0.0f, 0.0f);
+
+            // Ambijentna komponenta.
+            var ambientComponent = ambientLight.multiple(intersectionSphere.getKa());
+            // Dodavanje ambijentne komponente.
+            colorContributions = colorContributions.add(ambientComponent);
+
+            // Za izračun ostalih komponenti mi treba normalizirana normala presjeka.
+            var intersectionNormal = intersectionSphere.getNormal(intersection);
+            intersectionNormal.normalize();
+
+            // Ako nismo naišli na sjenu dodajemo difuznu i spekularnu komponentu, te
+            // doprinose od reflektirane i refraktirane zrake.
+            if (!shadow(intersection))
+            {
+                // Za izračun difuzne i spekularne komponente treba mi
+                // vektor od presjeka do izvora svjetla i normala presjeka.
+                var intersectionToLightSourceVector = new Vector(
+                    lightPosition.getX() - intersection.getX(),
+                    lightPosition.getY() - intersection.getY(),
+                    lightPosition.getZ() - intersection.getZ());
+                // Za prirodnije svjetlo normaliziram.
+                intersectionToLightSourceVector.normalize();
+
+                // Difuzna komponenta.
+                // Izračun difuzne komponente.
+                var diffuseComponent = light
+                    .multiple(intersectionSphere.getKd())
+                    .multiple(intersectionToLightSourceVector.dotProduct(intersectionNormal));
+                // Treba ispraviti difuznu komponentu da ne poništava ambijentnu kad bude pretamna.
+                diffuseComponent.correct();
+
+                // Spekularna komponenta.
+                // Za spekularnu komponentu mi treba zraka od izvora svjetla odbijena od presjeka.
+                var reflectedLightRay = intersectionToLightSourceVector
+                    .multiple(-1).getReflectedVector(intersectionNormal);
+                // Za prirodnije osvjetljenje normaliziram.
+                reflectedLightRay.normalize();
+                // Trebam izračunati projekciju odbijene zrake na smjer praćene zrake.
+                var reflectionProjection = reflectedLightRay.dotProduct(
+                    ray.getDirection());
+
+                // Izračun spekularne komponente.
+                var specularComponent = light
+                    .multiple(intersectionSphere.getKs())
+                    .multiple(Math.Pow(reflectionProjection, intersectionSphere.getN()));
+                // Ovaj put ispravljam spekularnu komponentu da ne nadjača doprinose
+                // difuzne i ambijentne.
+                specularComponent.correct();
+
+                // Računam zbroj svih doprinosa od svjetla.
+                var lightContributions = diffuseComponent
+                    .add(specularComponent);
+                // Ispravljam doprinose od svjetla jer mogu poništiti ambijentnu komponentu.
+                lightContributions.correct();
+
+                // Dodajem doprinose od svjetla doprinosima boje.
+                colorContributions = colorContributions
+                    .add(lightContributions);
+            }
+
+            // Računanje doprinosa reflektiranih i refraktiranih zraka.
+            // Da bih dobio doprinose reflektirane i refraktirane zrake trebam ih pratiti i
+            // povećati dubinu za jedan.
+            // Smjer reflektirane zrake.
+            var reflectedRayDirection = ray.getDirection()
+                .getReflectedVector(intersectionNormal);
+            // Doprinos reflektirane zrake.
+            var reflectionContribution = traceRay(
+                    new Ray(intersection, reflectedRayDirection),
+                    depth + 1)
+                .multiple(intersectionSphere.getReflectionFactor());
+
+            // Smjer refraktirane zrake.
+            var refractedRayDirection = ray.getDirection().getRefractedVector(
+                intersectionNormal,
+                intersectionSphere.getNi());
+            // Doprinos refraktirane zrake.
+            var refractionContribution = traceRay(
+                    new Ray(intersection, refractedRayDirection),
+                    depth + 1)
+                .multiple(intersectionSphere.getRefractionFactor());
+
+            // Zbrajam doprinose rekurzivnih poziva i
+            // ispravljam ih da ne ponište ostale doprinose.
+            var recursionContribution = reflectionContribution;
+            recursionContribution.correct();
+            // Na kraju ih dodajem ostalim komponentama.
+            colorContributions = colorContributions.add(recursionContribution);
+
+            // Zbroj doprinosa treba još samo ispraviti da je u granicama [0, 1] ako
+            // smo na početnoj dubini.
+            colorContributions.correct();
+
+            return colorContributions;
+        }
+    }
+}
---
+++ exercise2/RayTracing/RayTracing/Sphere.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace raytracing
+{
+    /// <summary>
+    /// Klasa predstavlja kuglu u prostoru. Nasljeduje apstraktnu klasu Object. Kugla
+    /// je odredena svojim polozajem, radijusom, bojom, parametrima materijala i
+    /// udjelima pojedninih zraka (osnovne, odbijene i lomljene).
+    /// </summary>
+    public class Sphere:Object
+    {
+        private double radius;
+        const double Epsilon = 0.0001;
+        private Point IntersectionPoint;
+
+        /// <summary>
+        /// Inicijalni konstruktor koji postavlja sve parametre kugle. Za prijenos
+        /// parametara koristi se pomocna klasa SphereParameters.
+        /// </summary>
+        /// <param name="sphereParameters">parametri kugle</param>
+        public Sphere ( SphereParameters sphereParameters )
+            : base(sphereParameters.getCenterPosition(), sphereParameters.getRaysContributions(),
+                sphereParameters.getMaterialParameters(), sphereParameters.getN(),
+                sphereParameters.getNi())
+        {
+            this.radius = sphereParameters.getRadius();
+        }
+
+        /// <summary>
+        /// Metoda ispituje postojanje presjeka zrake ray s kuglom. Ako postoji presjek
+        /// postavlja tocku presjeka IntersectionPoint, te
+        /// vraca logicku vrijednost true.
+        /// </summary>
+        /// <param name="ray">zraka za koju se ispituje postojanje presjeka sa kuglom</param>
+        /// <returns>logicku vrijednost postojanja presjeka zrake s kuglom</returns>
+        public override bool intersection ( Ray ray )
+        {
+            // Išao sam metodom da nađem parametarsku jednadžbu linije zrake i
+            // jednadžbu ravnine kojoj je normala vektor od centra sfere do ishodišta zrake, te
+            // nađem točku njihovog presjeka, pa ako je udaljenost od te točke do radiusa
+            // manja od radijusa postoji presjek, a ako ne, presjek ne postoji.
+
+            // Prvo mi trebaju vektor centra sfere i vektor ishodišta zrake.
+            var sphereCenterVector = new Vector(
+                centerPosition.getX(),
+                centerPosition.getY(),
+                centerPosition.getZ());
+            var rayStartVector = new Vector(
+                ray.getStartingPoint().getX(), 
+                ray.getStartingPoint().getY(), 
+                ray.getStartingPoint().getZ());
+
+            // Spremam normalu u varijablu da ne stvaram više istih vektora.
+            var planeNormal = this.getNormal(ray.getStartingPoint());
+
+            // Računam parametar koji ću uvrstiti u parametarsku jednadžbu linije zrake.
+            // Slika kako sam došao do ovoga je u intersection.jpg
+            var lineParameter = 
+                planeNormal.dotProduct(sphereCenterVector.sub(rayStartVector)) /
+                planeNormal.dotProduct(ray.getDirection());
+            
+            // Trebam još provjeriti, zbog ove metode, da li je točka na liniji u smjeru vektora zrake.
+            // Ako je u smjeru vektora zrake, onda je parametar linije zrake pozitivan.
+            if (lineParameter < 0) return false;
+
+            // Uvrstim parametar u jednadžbu linije i iz nje dobim vektor točke presjeka s ravninom.
+            var planePoint = rayStartVector.add(ray.getDirection().multiple(lineParameter));
+
+            // Računam udaljenost od centra sfere do točke presjeka na ravnini.
+            var planePointDistance = planePoint.sub(sphereCenterVector).getLength();
+
+            // Ako je udaljenost veća od radijusa, nema presjeka.
+            if (planePointDistance > radius) return false;
+            
+            // Sad kada imam presjek ravnine, samo se trebam pomaknuti u
+            // smjeru obrnutom od smjera zrake za sqrt(radius ^ 2 - udaljenost ^ 2)
+            // za bliži presjek, a za dalji u smjeru zrake.
+            var step = Math.Sqrt(radius * radius - planePointDistance * planePointDistance);
+
+            // Ako je zraka unutar sfere trebamo odabrati dalji presjek, a ako je van sfere onda bliži.
+            // Po uputama koristim epsilon jer bi se, na primjer, refraktirana zraka
+            // mogla nalaziti točno na granici sfere
+            var direction = 
+                ray.getStartingPoint().getDistanceFrom(centerPosition) < radius + Epsilon ? 
+                ray.getDirection() :
+                // Jedinični vektor obrnutog smjera dobimo množenjem jediničnog vektora s -1.
+                ray.getDirection().multiple(-1);
+
+            // Pretvaranje u točku.
+            IntersectionPoint = new Point(
+                planePoint.getX() + direction.getX() * step,
+                planePoint.getY() + direction.getY() * step,
+                planePoint.getZ() + direction.getZ() * step);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Vraca tocku presjeka kugle sa zrakom koja je bliza pocetnoj tocki zrake.
+        /// </summary>
+        /// <returns>tocka presjeka zrake s kuglom koja je bliza izvoru zrake</returns>
+        public override Point getIntersectionPoint ()
+        {
+            return IntersectionPoint;
+        }
+
+        /// <summary>
+        /// Vraca normalu na kugli u tocki point
+        /// </summary>
+        /// <param name="point">point na kojoj se racuna normala na kugli</param>
+        /// <returns>normal vektor normale</returns>
+        public override Vector getNormal ( Point point )
+        {
+            return new Vector(centerPosition, point);
+        }
+    }
+}
